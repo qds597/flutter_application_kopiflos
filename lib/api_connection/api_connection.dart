15d733b819bf2ff5e0dd0bc59c0e5b69d8f1bfbf
2212010012 <muhammadqaddasa@gmail.com>
2024-08-06 18:16:24 +0700

    f

--- lib/api_connection/api_connection.dart
+++ lib/api_connection/api_connection.dart
@@ -1,9 +1,9 @@
 class API {
-  static const hostConnect = "http://192.168.128.88/api_kopiflos_store";
+  static const hostConnect = "http://192.168.129.83/api_kopiflos_store";
   static const hostConnectUser = "$hostConnect/user";
 
   //signUp user
   static const validateEmail = "$hostConnect/user/validate_email.php";
-  static const signUp = "$hostConnect/user/signup.php";
+  static const signUp = "$hostConnect/user/signUp.php";
   static const login = "$hostConnect/user/login.php";
 }
